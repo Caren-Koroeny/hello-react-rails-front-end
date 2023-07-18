59ec31735b0c1620790ccee4e63c845b5de118fc
Caren-Koroeny <carensiya5@gmail.com>
2023-07-18 12:31:27 +0300

    Added store.js and greeting.js files

---
+++ src/components/Greeting.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greeting/greetingSlice';
+
+const Greeting = () => {
+  const { message, isLoading } = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  const handleClick = () => {
+    dispatch(fetchGreeting());
+  };
+
+  if (isLoading) {
+    return <h1 style={{ textAlign: 'center' }}>Loading...</h1>;
+  }
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ color: 'blue' }}>{message}</h1>
+      <button
+        style={{
+          padding: '10px',
+          background: 'green',
+          color: 'white',
+          borderRadius: '5px',
+          cursor: 'pointer',
+        }}
+        onClick={handleClick}
+      >
+        Random Greeting
+      </button>
+    </div>
+  )  
+};
+
+export default Greeting;
